918ff6993fe0b274be3729778238b01b4af3aae5
Dylan Britz <britzdylan@gmail.com>
2025-04-13 22:20:01 +0200

    feat: enhance site configuration and add blog functionality with new components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+import vue from '@astrojs/vue';
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://dylanbritz.dev',
+  trailingSlash: 'ignore',
+  integrations: [vue(), mdx()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
