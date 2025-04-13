fc95849cea608ce30463a3fc1abe61a22e9444c3
Dylan Britz <britzdylan@gmail.com>
2025-04-14 00:35:45 +0200

    feat: add blog card and header components; remove unused card component

---
+++ src/components/ui/blog-card.vue
@@ -0,0 +1,34 @@
+<template>
+  <motion.li
+    class="pb-6 border-b border-neutral-300 flex flex-col gap-2 cursor-pointer group hover:border-neutral-500"
+    :initial="{ opacity: 0, y: 20 }"
+    :whileInView="{ opacity: 1, y: 0 }"
+    :inViewOptions="{ once: true }"
+    :transition="{ duration: 0.3, delay: delay * 0.1, ease: 'easeIn' }">
+    <a :href="url">{{ title }}</a>
+    <div class="flex gap-2">
+      <time class="text-sm text-neutral-500">
+        {{ date.toLocaleDateString() }}
+      </time>
+      <span class="text-sm text-neutral-500"> • </span>
+      <span class="text-sm text-neutral-500">{{ author }}</span>
+    </div>
+  </motion.li>
+</template>
+
+<script setup lang="ts">
+  import { motion } from 'motion-v';
+  import { defineProps } from 'vue';
+
+  type Props = {
+    title: string;
+    date: Date;
+    author: string;
+    delay: number;
+    url: string;
+  };
+
+  defineProps<Props>();
+</script>
+
+<style scoped></style>
---
+++ src/components/ui/blog-header.vue
@@ -0,0 +1,25 @@
+<template>
+  <motion.div
+    :initial="{ width: 0 }"
+    :whileInView="{ width: '100%', height: 'auto' }"
+    :inViewOptions="{ once: true }"
+    :transition="{ duration: 1, delay: 0.2, ease: 'easeOut' }"
+    class="w-full border-t-2 border-neutral-800 sticky top-4 pt-1 overflow-hidden">
+    <h1 class="text-2xl md:text-3xl font-medium">
+      {{ title }}
+    </h1>
+  </motion.div>
+</template>
+
+<script setup lang="ts">
+  import { motion } from 'motion-v';
+  import { defineProps } from 'vue';
+
+  type Props = {
+    title: string;
+  };
+
+  defineProps<Props>();
+</script>
+
+<style scoped></style>
